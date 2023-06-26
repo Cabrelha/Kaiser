fd2fc1fff589eaa4a8f8c274cd6f5dbf11a37f2a
Cabrelha <83473994+Cabrelha@users.noreply.github.com>
2023-06-26 19:27:59 -0300

    Add files via upload

---
+++ Kaiser/produtos.js
@@ -0,0 +1,57 @@
+var addBtn = document.querySelector("#addbottom a");
+var popup = document.querySelector("#popupbackground");
+var closeBtn = document.querySelector("#popup form a");
+var form = document.getElementById("pou") // <--tentando colocar um botão de envio
+
+addBtn.addEventListener("click", ()=>{
+    popup.style.display = "flex";
+    popup.style.transition = "display 5s";
+})
+
+closeBtn.addEventListener("click", ()=>{
+    popup.style.display = "none";
+    popup.style.transition = "display 5s";
+})
+// botão 2
+
+
+
+
+// tentando colocar um botão de envio
+var closeBtn = document.querySelector("#env");
+
+closeBtn.addEventListener("click", ()=>{
+    popup.style.display = "none";
+    popup.style.transition = "display 5s";
+})
+
+var recebidos =
+  JSON.parse(localStorage.getItem("@sonhosdepelucia/recebido")) || [];
+var contato = document.getElementById("popup");
+
+form.addEventListener("submit",(event)=>{
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const info = Object.fromEntries(formData);
+
+  recebidos.push(info);
+  localStorage.setItem("@sonhosdepelucia/recebido", JSON.stringify(recebidos));
+  alert("Mensagem enviada com sucesso!");
+  form.reset();
+});
+
+
+
+// // Can also be used with $(document).ready()
+// $(window).load(function() {
+//     $('.flexslider').flexslider({
+//       animation: "slide"
+//     });
+//   });
+// // Can also be used with $(document).ready()
+// $(window).load(function() {
+//     $('.flexslider').flexslider({
+//       animation: "slide"
+//     });
+//   });
